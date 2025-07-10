692ea3ff61200e0be65a621433157359543a06bc
glebkoxan36 <glebkoxan36@gmail.com>
2025-07-10 21:06:23 +0400

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
     SESSION_COOKIE_SAMESITE='Lax',
     PERMANENT_SESSION_LIFETIME=timedelta(days=7)
+)
 Session(app)
 
 # Инициализация CSRF защиты
