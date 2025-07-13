d5ca4ad55e4cbb4a497f7e7077da4b2529e61678
glebkoxan36 <glebkoxan36@gmail.com>
2025-07-13 06:29:09 +0400

    Update mobilemenu.js

--- static/js/mobilemenu.js
+++ static/js/mobilemenu.js
@@ -59,7 +59,7 @@
             bottom: 0;
             left: 0;
             width: 100%;
-            height: 65vh;
+            height: 60vh;
             max-height: none;
             z-index: 1100;
             align-items: flex-end;
