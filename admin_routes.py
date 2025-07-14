782b2e40542adadb0bffe95ba1882c900572d789
glebkoxan36 <glebkoxan36@gmail.com>
2025-07-14 10:20:44 +0400

    Update admin_routes.py

--- admin_routes.py
+++ admin_routes.py
@@ -97,7 +97,7 @@ def price_management():
             try:
                 # ИСПРАВЛЕНИЕ: правильное преобразование данных формы
                 paid_price = int(float(request.form.get('paid_price')) * 100)
-                premium_price = int(float(request.form.get('premium_price')) * 100
+                premium_price = int(float(request.form.get('premium_price')) * 100)
                 
                 current_doc = prices_collection.find_one({'type': 'current'})
                 current_prices = current_doc['prices']
