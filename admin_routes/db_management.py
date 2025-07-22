912cb19c0e0f234475984ab3677bc743c125d39e
glebkoxan36 <glebkoxan36@gmail.com>
2025-07-23 03:07:26 +0400

    Update db_management.py

--- admin_routes/db_management.py
+++ admin_routes/db_management.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, flash, redirect, url_for, current_app
-from . import admin_bp
+from .bp import admin_bp
 from .auth_decorators import admin_required
 from bson import ObjectId
 from db import db, client
