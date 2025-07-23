7d793adca27851fb08ea98736a66d3e20588dd21
glebkoxan36 <glebkoxan36@gmail.com>
2025-07-23 04:04:03 +0400

    Update db.py

--- db.py
+++ db.py
@@ -14,8 +14,8 @@ logger.setLevel(logging.INFO)
 def init_logger():
     """Инициализация логгера для модуля"""
     if not os.path.exists('logs'):
-      try: 
-        os.makedirs('logs, exist_ok=True')
+      
+        os.makedirs(logs, exist_ok=True)
     handler = logging.FileHandler('logs/db.log')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
